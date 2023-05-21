1a6de111277ccfe335f8c75c88d94d4447ea8f62
Ylixandr <savoshinsky@inbox.ru>
2023-05-21 15:56:21 +0300

    Добавьте файлы проекта.

---
+++ Task_16_PolessUP/Program.cs
@@ -0,0 +1,18 @@
+﻿{
+    static bool CheckPowersOfThree(int n)
+    {
+        while (n > 0)
+        {
+            int remainder = n % 3;
+            if (remainder == 2)
+            {
+                return false;
+            }
+            n /= 3;
+        }
+        return true;
+
+    }
+    int n = 12;
+    Console.WriteLine(CheckPowersOfThree(n));
+}
